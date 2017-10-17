e041a4769e9a4d2f24d03e2f0732b28cfb32dbf3
Rachelle Dauphin <rachdaup.tech@gmail.com>
2017-10-17 19:23:19 -0400

    Modify the edit form, so payment log updates after modification. Add jQuery variable to edit component.

--- code/client/src/app/components/bank-edit/bank-edit.component.ts
+++ code/client/src/app/components/bank-edit/bank-edit.component.ts
@@ -6,6 +6,7 @@ import { BankCalulationsService } from '../../services/bank-calulations.service'
 import { AuthApiService } from '../../services/auth-api.service';
 
 import * as moment from 'moment';
+declare var $:any;
 
 @Component({
   selector: 'app-bank-edit',
@@ -21,6 +22,7 @@ export class BankEditComponent implements OnInit {
   errorMessage: string;
     constructor(
       private bank: BankApiService,
+      private calc: BankCalulationsService,
       private auth: AuthApiService
     ) {
     }
@@ -28,7 +30,16 @@ export class BankEditComponent implements OnInit {
     ngOnInit() {
     }
 
+    ngAfterViewInit(){
+      $('select').material_select();
+    }
+
   bankInfoObject(){
+    this.bankInfo.payments.remainingCost = this.bankInfo.totalValue;
+    this.bankInfo.dateFormatted = moment(this.bankInfo.date).format("dddd, MMMM Do YYYY");
+    this.bankInfo.payments.paymentLog = this.calc.numberPeriod(this.bankInfo);
+    this.bankInfo.payments.numberPaymentsLeft = this.bankInfo.payments.paymentLog.length;
+
     this.bank.editBank(this.bankInfo)
       .subscribe(
         (bankDetails: any) =>{
