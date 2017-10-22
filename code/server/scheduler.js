73b156bfdae0fe728d31d635caec521b5d4f2648
Rachelle Dauphin <rachdaup.tech@gmail.com>
2017-10-22 18:19:12 -0400

    Modify job scheduler to run everyday at 7:30 AM instead of every second.

--- code/server/scheduler.js
+++ code/server/scheduler.js
@@ -7,7 +7,8 @@ const moment = require('moment');
 const schedulerFactory = function() {
   return {
     start: function() {
-      new CronJob('00 * * * * *', function() {
+  // Runs every day at 7:30:00 AM.
+      new CronJob('00 30 7 * * *', function() {
         console.log('Running Send Notifications Worker for ' +
           moment().format());
         notificationsWorker.run();
